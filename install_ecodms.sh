77bb3df351693ed3d22c44a13ca1ec22dd87b408
bigTracy <steve.red@gmx.net>
2020-08-06 12:09:39 +0200

    Kommentare ergänzt

--- install_ecodms.sh
+++ install_ecodms.sh
@@ -13,7 +13,7 @@ then
 		echo "###################################################"
 fi
 
-
+# needed configs for succesfull installation
 $prefix apt update
 $prefix export LANG=de_DE.UTF8
 $prefix dpkg-reconfigure locales
@@ -21,9 +21,11 @@ $prefix dpkg-reconfigure tzdata
 
 $prefix apt upgrade -y
 $prefix apt install sudo gnupg2 -y
+
+# prepare ecodms installation
 $prefix wget -O - http://www.ecodms.de/gpg/ecodms.key | sudo apt-key add -
 $prefix echo $ecodms > /etc/apt/sources.list.d/ecodms.list
-
 $prefix apt update
 
+# install ecodmsserver
 $prefix apt install ecodmsserver -y
